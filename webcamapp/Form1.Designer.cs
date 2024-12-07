6c2c54ae0d0a412aca7ed42e2a0d1e109cd2d90f
nazlisayar <verassyr81@gmail.com>
2024-12-07 22:33:16 +0300

    Proje dosyası ekle.

---
+++ webcamapp/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace webcamapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btncapture = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(25, 60);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(999, 493);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btncapture
+            // 
+            this.btncapture.Location = new System.Drawing.Point(891, 12);
+            this.btncapture.Name = "btncapture";
+            this.btncapture.Size = new System.Drawing.Size(124, 42);
+            this.btncapture.TabIndex = 1;
+            this.btncapture.Text = "CAPTURE";
+            this.btncapture.UseVisualStyleBackColor = true;
+            this.btncapture.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 632);
+            this.Controls.Add(this.btncapture);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btncapture;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ webcamapp/Form1.cs
@@ -0,0 +1,95 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace webcamapp
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices; 
+        private VideoCaptureDevice videoSource;
+        private Bitmap currentFrame;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            capturephoto();
+        }
+        private void capturephoto()
+        {
+
+            if (currentFrame != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "JPEG Image|*.jpg";
+                saveFileDialog.Title = "fotoğrafı kaydet";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK) 
+                { 
+                    currentFrame.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);//foto kaydet
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("webcam görüntüsü alınamadı");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        //form yüklendiğinde webcami başlatması için;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            videoDevices= new FilterInfoCollection(FilterCategory.VideoInputDevice);   //video cihazlarını al
+            if (videoDevices.Count == 0) 
+            {
+                MessageBox.Show("webcam bulunamadı");
+                return;
+            }
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += new NewFrameEventHandler(videoSource_Newframe);
+            videoSource.Start();    
+        }
+        private void videoSource_Newframe(object sender,NewFrameEventArgs eventArgs)
+        {
+             currentFrame=(Bitmap)eventArgs.Frame.Clone();   //anlık görüntü alma
+          
+            pictureBox1.Image =  new Bitmap(currentFrame, pictureBox1.Width,pictureBox1.Height);
+            
+
+        }
+        private void Form1_Formclosing(object sender, FormClosingEventArgs e)
+        {
+            if(videoSource.IsRunning)
+            { videoSource.SignalToStop(); 
+              videoSource.WaitForStop();
+            }
+        }
+    }
+}
